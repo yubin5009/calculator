c2bb601271b211f41f58ed5034a39a6b955886ec
정유빈 <127919170+yubin5009@users.noreply.github.com>
2024-12-14 02:33:05 +0900

    202313466 정유빈 - stack.h 수정
    202313466 정유빈 - stack.h 수정

--- stack.h
+++ stack.h
@@ -16,6 +16,7 @@ bool is_full(Stack *s);
 void push(Stack *s, Item i);
 Item pop(Stack *s);
 Item peek(Stack *s);
+// 1줄 띄어쓰기 수정
 static void stack_underflow(void);
 static void reallocate(Stack * stack);
 #endif
